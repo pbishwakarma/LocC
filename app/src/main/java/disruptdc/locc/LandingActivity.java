114859cf0234ab78dacdfcd913863eecbbef7c23
Ben Dykstra <bfdykstra@email.wm.edu>
2017-02-11 15:53:30 -0500

    Added button to go to google maps

--- app/src/main/java/disruptdc/locc/LandingActivity.java
+++ app/src/main/java/disruptdc/locc/LandingActivity.java
@@ -1,8 +1,12 @@
 package disruptdc.locc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -13,5 +17,17 @@ public class LandingActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_landing);
+
+        Button testButton = (Button) findViewById(R.id.button2);
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LandingActivity.this, MapsActivity.class));
+            }
+        });
+
     }
+
+
 }
