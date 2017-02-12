96ec1118752c299faac19ffd5af167c87b039227
turner <turner@notch.bz>
2017-02-12 03:26:15 -0500

    finished SettingsActivity, set up LeaderSelectionActivity shell

--- app/src/main/java/disruptdc/locc/components/DataStorage.java
+++ app/src/main/java/disruptdc/locc/components/DataStorage.java
@@ -4,14 +4,12 @@ package disruptdc.locc.components;
  * Created by turner on 2/11/17.
  */
 public class DataStorage {
-    // boolean saying that user has successfully dropped pin
-    // will ultimately be changed to location-related object
-    public static boolean locationSelected;
     public static boolean BenBoolean;
     public static boolean TurnerBoolean;
     public static boolean PrajalBoolean;
     public static boolean SallyBoolean;
     public static boolean TimmyBoolean;
+
     public static boolean teamLeader;
 
     public static int radius;
